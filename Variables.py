3f71d9273ca60abbcaa3c4b314452efede7be8e4
UsmanTF <usmangolubtsov@gmail.com>
2024-11-19 02:05:17 +0500

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,5 @@
+quantity_of_homework = 12
+quantity_of_hours = 1.5
+course_name = 'Python'
+time_for_one_task = quantity_of_homework / quantity_of_hours
+print('Курс:',course_name,', всего задач:',quantity_of_homework,', затрачено часов:',quantity_of_hours,', среднее время выполнения:',int(time_for_one_task),'часов.')
