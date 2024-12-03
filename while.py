3dc09c6af134cc8a3861700f002bd374351f2ce4
UsmanTF <usmangolubtsov@gmail.com>
2024-12-04 02:30:09 +0500

    Add files via upload

---
+++ while.py
@@ -0,0 +1,10 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+var = my_list[a]
+while True:
+    if my_list[a] == 0:
+        a += 1
+        continue
+    elif my_list[a] > 0 and a < len(my_list):
+        print(my_list[a])
+        a += 1
