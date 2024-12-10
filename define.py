258f9b61c180ad6aeb498e4a455e83977de72f1d
UsmanTF <usmangolubtsov@gmail.com>
2024-12-11 03:18:55 +0500

    Add files via upload

---
+++ define.py
@@ -0,0 +1,10 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+
+matrix = get_matrix()
+print(matrix)
